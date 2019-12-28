ebe4a17a0bb1f35e1b0b49351dde36b1ae1a2d01
Anthony Li <ali04@stuy.edu>
2019-12-27 21:45:14 -0500

    fixes

--- control.c
+++ control.c
@@ -26,6 +26,11 @@ int main(int argc, char * argv[]){
   int shm;
   int sem;
   int fd;
+  union semun su;
+  su.val = 1;
+  struct sembuf sb;
+  sb.sem_num = 0;
+  sb.sem_op = -1;
   if(argc < 2){
     printf("Error: missing flag\n");
     return 0;
@@ -33,27 +38,24 @@ int main(int argc, char * argv[]){
   if (!strcmp(argv[1], "-c")){
     shm = shmget(KEY, SIZE, IPC_CREAT | 0644);
     errcheck("creating shared memory");
+    printf("Shared Memory created\n");
     sem = semget(KEY, 1, IPC_CREAT | 0644);
     errcheck("creating semaphore");
-    union semun s;
-    s.val = 1;
-    semctl(sem, 0, SETVAL, s);
+    printf("Semaphore created\n");
+    semctl(sem, 0, SETVAL, su);
     errcheck("setting semaphore");
-    fd = open("text.txt", O_CREAT | O_EXCL, 0644);
-    if (errno == EEXIST) errno = 0;
+    FILE * f = fopen("story", "w");
     errcheck("creating file");
-    close(fd);
+    fclose(f);
+    printf("File created\n");
   }
   else if(!strcmp(argv[1], "-r")){
     sem = semget(KEY, 1, 0);
     errcheck("getting semaphore");
-    struct sembuf sb;
-    sb.sem_num = 0;
-    sb.sem_op = -1;
     printf("waiting...\n");
     semop(sem, &sb, 1);
     errcheck("getting semaphore");
-    FILE* f = fopen("text.txt", "r");
+    FILE* f = fopen("story", "r");
     errcheck("opening file");
     printf("The Story:\n");
     char line[SIZE];
@@ -63,7 +65,7 @@ int main(int argc, char * argv[]){
     }
     printf("\n");
     fclose(f);
-    close(open("text.txt", O_WRONLY | O_TRUNC));
+    close(open("story", O_WRONLY | O_TRUNC));
     shm = shmget(KEY, SIZE, 0);
     errcheck("getting shared memory");
     shmctl(shm, IPC_RMID, 0);
@@ -73,7 +75,7 @@ int main(int argc, char * argv[]){
     printf("Removal complete\n");
   }
   else if(!strcmp(argv[1], "-v")){
-    FILE* f = fopen("text.txt", "r");
+    FILE* f = fopen("story", "r");
     errcheck("opening file");
     printf("The story so far:\n");
     char line[SIZE];
